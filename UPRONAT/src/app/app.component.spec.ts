4ce6d3305816397fde8a571f29772540b31271ee
Mateo521 <mateovillalva42@gmail.com>
2024-04-30 23:29:58 -0300

    init commit

--- UPRONAT/src/app/app.component.spec.ts
+++ UPRONAT/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-UPRONAT' title`, () => {
+  it(`should have as title 'UPRONAT'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-UPRONAT');
+    expect(app.title).toEqual('UPRONAT');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-UPRONAT');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UPRONAT');
   });
 });
