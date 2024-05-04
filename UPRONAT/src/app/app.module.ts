fa962fdbd2db1288baeef27e89ddeaad8f90e207
Atahualpa Arias <atahulpq@gmail.com>
2024-05-04 18:43:09 -0300

    Crear carpeta de componentes y Home page

--- UPRONAT/src/app/app.module.ts
+++ UPRONAT/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
