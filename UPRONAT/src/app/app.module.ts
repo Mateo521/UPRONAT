e7209d837e8f8245dc81ed2bf8dee562592db7ee
Atahualpa Arias <atahulpq@gmail.com>
2024-05-05 15:54:27 -0300

    Crear página principal y la estructura

--- UPRONAT/src/app/app.module.ts
+++ UPRONAT/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { ContactComponent } from './components/home/contact/contact.component';
+import { ServiciosComponent } from './components/home/servicios/servicios.component';
+import { BannerComponent } from './components/home/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServiciosComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ UPRONAT/src/app/components/home/servicios/servicios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-servicios',
+  templateUrl: './servicios.component.html',
+  styleUrl: './servicios.component.css'
+})
+export class ServiciosComponent {
+
+}
