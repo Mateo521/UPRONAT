547d5c823facfd3db9cc9da00db0f585251b9f4c
Mateo521 <mateovillalva42@gmail.com>
2024-05-12 14:19:09 -0300

    Instalacion de AOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'UPRONAT';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({ once: true, duration: 1000 });
+    }
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
+interface logo {
+  imageUrl: string
+  description: string
+}
+let baseUrl: string = "/assets/Logos_Svg/"
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
+
+
+
 export class HeaderComponent {
+
+  imgs: logo[] = [
+    {imageUrl: baseUrl + '7.svg', description: 'Logo Dark' },
+    {imageUrl: baseUrl + '8.png', description: 'Logo' },
+   ]
+
   
 }
