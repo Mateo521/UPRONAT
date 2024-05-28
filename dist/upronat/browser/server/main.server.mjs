4de97c156438e4de4b7d05de42c20a28b920b4be
Mateo521 <mateovillalva42@gmail.com>
2024-05-27 22:12:48 -0300

    fixs

--- dist/upronat/browser/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-LGGBCEQA.mjs";import"./chunk-TGYQ277I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- dist/upronat/server/main.server.mjs
+++ dist/upronat/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LL7TQZTG.mjs";import"./chunk-XQNPW6JU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LGGBCEQA.mjs";import"./chunk-TGYQ277I.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
