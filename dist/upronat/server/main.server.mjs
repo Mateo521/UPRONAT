48f07d247bdbc6a3653edc090656ce7ec88b4db1
Mateo521 <mateovillalva42@gmail.com>
2024-06-07 21:54:07 -0300

    fixs

--- dist/upronat/server/main.server.mjs
+++ dist/upronat/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VCTIBCRJ.mjs";import"./chunk-XYAYHIQM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-UZ2VZJPI.mjs";import"./chunk-XYAYHIQM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/components/routes/que-podemos-hacer/vinos/vinos.component.ts
+++ src/app/components/routes/que-podemos-hacer/vinos/vinos.component.ts
@@ -29,7 +29,7 @@ export class VinosComponent  implements AfterViewInit {
 
   logoImg!: string;
   imageUrl!: string;
-  title: string = 'Analizamos vinos mediante una tecnología única en Sudamérica (<sup class="pl-1">1</sup>H-NMR Wine screening) que permite certificar variedad, denominación de origen, terroir, fraude o alteraciones e inscribirlos en una base de datos mundial. <br> Estamos trabajando para incorporar por primera vez al Malbec argentino a la base de datos mundial de vinos por RMN. ';
+  title: string = 'Analizamos vinos mediante una tecnología única en Sudamérica (<sup class="pl-1">1</sup>H-NMR Wine screening) que permite certificar variedad, denominación de origen, terroir, fraude o alteraciones. ';
   url:string = 'Vinos';
   description:string = 'Qué podemos hacer';
   constructor(private imageService: ImageService) {}
@@ -89,8 +89,35 @@ export class VinosComponent  implements AfterViewInit {
     },
     {
       id: 5,
-      title: 'Determinación de la variedad',
+      title: 'Determinación de la variedad <br> <hr>',
       content: [''],
+      links: [
+        { text: '', href: '' },
+        { text: '', href: '' }
+      ]
+    },
+    {
+      id: 6,
+      title: '',
+      content: ['Sabemos que el origen y la autenticidad de los productos alimenticios están ganando cada vez más atención, y los consumidores exigen más información sobre el origen del vino. Desafortunadamente, la detección de vino fraudulento relacionado con declaraciones de origen, regiones o variedades de uva falsas ha sido un gran desafío debido a la falta de métodos analíticos disponibles. El sistema de screening mediante resonancia magnética nuclear es una forma innovadora y poderosa de detectar y prevenir el fraude en el vino.'],
+      links: [
+        { text: '', href: '' },
+        { text: '', href: '' }
+      ]
+    },
+    {
+      id: 7,
+      title: '',
+      content: ['<b>¿Cómo funciona?</b> <br> Debido a sus exclusivas capacidades "todo en uno", la espectroscopia de <sup class="pl-1">1</sup>H-NMR de alta resolución, combinada con la quimiometría estadística multivariada, es una poderosa herramienta para probar la autenticidad y la calidad del vino. Wine-Profiling puede detectar varios cientos de compuestos en una muestra de vino simultáneamente, que se utilizan para caracterizar el vino a través de una base de datos completa de vinos de referencia. La base de datos se actualiza periódicamente con nuevas variantes para la verificación geográfica y de variedades de uva, lo que proporciona una biblioteca de referencia confiable y actualizada.'],
+      links: [
+        { text: '', href: '' },
+        { text: '', href: '' }
+      ]
+    },
+    {
+      id: 8,
+      title: '',
+      content: ['<b>¿Qué informamos?</b> <br> Además de la cuantificación de los principales compuestos orgánicos presentes en el vino, la metodología tiene la capacidad de verificar la consistencia de la variedad de uva informada en vinos monovarietales y de la denominación de origen. Podés encontrar un ejemplo de informe en el siguiente link  <a href="/assets/files/wine_report.pdf" class="inline-flex items-center font-medium text-emerald-600 dark:text-emerald-500 hover:underline">Wine Report.<svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/></svg></a> <br>  También tenés disponible un folleto en el siguiente link. <a href="/assets/files/WineProfilingBrochure.PDF" class="inline-flex items-center font-medium text-emerald-600 dark:text-emerald-500 hover:underline">Wine Profiling Brochure.<svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/></svg></a> '],
       links: [
         { text: '', href: '' },
         { text: '', href: '' }
@@ -101,7 +128,7 @@ export class VinosComponent  implements AfterViewInit {
 
 
   files: Logo[] = [
-    { imageUrl: baseUrl + 'WineProfilingBrochure.PDF', description: 'WineProfilingBrochure PDF' },
+    { imageUrl: baseUrl + 'WineProfilingBrochure.PDF', description: 'Wine Profiling Brochure PDF' },
     { imageUrl: baseUrl + 'wine_report.pdf', description: 'Wine Report PDF' },
    // { imageUrl: baseUrl + 'VinosporRMN.docx', description: 'Vinos por RMN DOCX' }
   ];
