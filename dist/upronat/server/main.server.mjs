98107f8fd0ef98a2adf71582709ee82c9d88a4d3
Mateo521 <mateovillalva42@gmail.com>
2024-07-03 10:43:57 -0300

    fix

--- dist/upronat/server/main.server.mjs
+++ dist/upronat/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FGDAA57V.mjs";import"./chunk-VELJOP53.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ESJBRWAA.mjs";import"./chunk-VELJOP53.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
